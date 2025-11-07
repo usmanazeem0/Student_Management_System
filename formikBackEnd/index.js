048481cba66d14b70b151b379a438c18974f23c0
usmanazeem0 <usmanmian7289@gmail.com>
2025-11-07 23:48:48 +0500

    added the link of frontend deployment

--- formikBackEnd/index.js
+++ formikBackEnd/index.js
@@ -7,7 +7,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://learning-m-system.netlify.app"],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
